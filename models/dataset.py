b5bfad5bc2f8b6af424cde6b49909399f5977d29
nambvarun <nambvarun@gmail.com>
2020-05-25 17:30:31 -0700

    refactor: cleaning up import for pylint

--- models/dataset.py
+++ models/dataset.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import struct
 import torch
+from torch import Tensor
 from torch.utils.data import Dataset
 
 
@@ -96,7 +97,7 @@ def exr_to_tensor(exr_filepath: str, half: bool) -> torch.Tensor:
     if half:
         img_array = img_array[:image.height//2, :image.width//2, :]
 
-    return torch.tensor(np.transpose(img_array, (2, 0, 1)), dtype=torch.float32)
+    return Tensor(np.transpose(img_array, (2, 0, 1)), dtype=torch.float32)
 
 
 def convert_exrs_to_tensors(src: str, tgt: str):
