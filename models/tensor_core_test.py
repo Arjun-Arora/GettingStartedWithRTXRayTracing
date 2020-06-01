f2fb604e7ccd96f213e08d62f52f0005249cd3dd
Arjun-Arora <aarora52@cs348k-vm.us-west1-b.c.rapid-rarity-278818.internal>
2020-06-01 00:48:24 +0000

    added modification to tensor_core_test.py to test espcn network

--- models/tensor_core_test.py
+++ models/tensor_core_test.py
@@ -1,17 +1,19 @@
 import torch
+from supersample_model import ESPCN
+from tqdm import tqdm
+input_tensor_shape= (1,14, 1060, 1920)
+output_tensor_shape = (1,3,1060,1920)
 
-N, D_in, D_out = 64, 1024, 512
+x = torch.randn(*input_tensor_shape).cuda().half()
+y = torch.randn(*output_tensor_shape).cuda().half()
 
-x = torch.randn(N, D_in).cuda().half()
-y = torch.randn(N, D_out).cuda().half()
-
-model = torch.nn.Linear(D_in, D_out).cuda().half()
-
+#model = torch.nn.Linear(D_in, D_out).cuda().half()
+model = ESPCN(1,14,3).cuda().half()
 opt = torch.optim.SGD(model.parameters(), lr=1e-3)
 
-for t in range(500):
+for t in tqdm(range(100)):
     y_pred = model(x)
-    
+    #print(y_pred.size())
     loss = torch.nn.functional.mse_loss(y_pred, y)
 
     opt.zero_grad()
